1b7ed21e4884fd5bb98874825b8310ea2cb157a0
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2023-05-26 14:36:03 +0200

    insa finish

--- lab5/main.py
+++ lab5/main.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from RandomNumberGenerator import RandomNumberGenerator
 
 import math
@@ -10,6 +11,7 @@ m3 = (3)
 
 
 def is_valid_pi(pi):
+
     for m_pi in pi:
         tasks = {}
         for tp in m_pi:
@@ -23,9 +25,20 @@ def is_valid_pi(pi):
     return True
 
 
+def is_pi_empty(pi):
+    for pi_temp in pi:
+        if len(pi_temp) != 0:
+            return False
+
+    return True
+
+
 def eval(p, pi, m):
     S = {}
     C = {}
+
+    if is_pi_empty(pi):
+        return [S, C, float("inf")]
 
     if not is_valid_pi(pi):
         return None
@@ -41,6 +54,7 @@ def eval(p, pi, m):
                 C.update({tp: -1})
                 S.update({tp: -1})
     while -1 in C.values():
+        changes = False
         for m_pi in pi:
             last = 0
             for tp in m_pi:
@@ -53,12 +67,36 @@ def eval(p, pi, m):
                                 [m_c[p[tp[0]][1][tp[1]]], C[(tp[0], tp[1] - 1)]])
                             C[tp] = S[tp] + p[tp[0]][0][tp[1]]
                             m_c[p[tp[0]][1][tp[1]]] = C[tp]
+                            changes = True
                     else:
                         S[tp] = m_c[p[tp[0]][1][tp[1]]]
                         C[tp] = S[tp] + p[tp[0]][0][tp[1]]
                         m_c[p[tp[0]][1][tp[1]]] = C[tp]
+                        changes = True
                 last = C[tp]
+        if not changes:
+            return None  # deadlock
     return [S, C, max(C.values())]
+
+
+def INSA(p, m):
+    tasks = [(i, a[1][0], 0) for i, a in enumerate(p)]
+    pi = [[] for _ in range(m)]
+    while len(tasks) != 0:
+        task = tasks.pop()
+        mach = task[1]
+        Cmax = float("inf")
+        temp_pi = deepcopy(pi)
+        for i in range(len(pi[mach]) + 1):
+            ttemp_pi = deepcopy(temp_pi)
+            ttemp_pi[mach].insert(i, (task[0], task[2]))
+            e = eval(p, ttemp_pi, m)
+            if (e != None) and (e[2] < Cmax):
+                Cmax = e[2]
+                pi = deepcopy(ttemp_pi)
+        if len(p[task[0]][1]) > task[2] + 1:
+            tasks.append((task[0], p[task[0]][1][task[2] + 1], task[2] + 1))
+    return [pi, Cmax]
 
 
 def init(j, m, seed=1):
@@ -67,17 +105,18 @@ def init(j, m, seed=1):
     for _ in range(j):
         oj = rng.nextInt(1, int(math.floor(m * 1.2)))
         p = [rng.nextInt(1, 29) for _ in range(oj)]
-        u = [rng.nextInt(0, m - 1) for _ in range(oj)]
+        u = [rng.nextInt(0, m - 1) for _ in enumerate(range(oj))]
         Z.append([p, u])
     return Z
 
 
 def main1():
-    Z = [[[2, 2, 2], [0, 1, 0]], [[3, 3, 3], [0, 1, 2]], [[4, 4], [2, 1]]]
-    pi = (((0, 0), (0, 2)), ((0, 1),), ((1, 1), ))
+    m = 10
+    Z = init(20, m)
     pprint(Z)
-    pprint(pi)
-    pprint(eval(Z, pi, 3))
+    pi, Cmax = INSA(Z, m)
+    print(pi, Cmax)
+    pprint(eval(Z, pi, m))
 
 
 if __name__ == '__main__':
